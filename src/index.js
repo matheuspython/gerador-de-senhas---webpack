97fc92f55d57f6437c7dc0ba5e0de8272d2552bb
matheus souza <ruivorobado@gmail.com>
2021-09-16 17:39:56 -0300

    fim

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
+import formGeraSenha from './modules/formGeraSenha'
+
 import './assets/css/resets.css'
 import './assets/css/style.css'
-import Senha from './modules/Senha'
 
-const senhaOutput = document.querySelector('.senha')
+formGeraSenha()
 
-const senha = new Senha(senhaOutput)
 
-document.addEventListener('click', e => {
-  const el = e.target;
-  if(el.classList.contains('start'))  senha.inicia()
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Senha from './modules/Senha'
+
+// const senhaOutput = document.querySelector('.senha')
+
+// const senha = new Senha(senhaOutput)
+
+// document.addEventListener('click', e => {
+//   const el = e.target;
+//   if(el.classList.contains('start'))  senha.inicia()
+// })
 
---
+++ src/modules/formGeraSenha.js
@@ -0,0 +1,28 @@
+import geraSenha from "./geradoes";
+
+const senhaGerada = document.querySelector(".senha-gerada")
+const qtdCaracteres = document.querySelector('.qtd-caracteres')
+
+const chkMaiusculas = document.querySelector(".chk-maiusculas")
+const chkMinusculas =document.querySelector(".chk-minusculas")
+const chkNumeros =document.querySelector(".chk-numeros")
+const chkSimbolos =document.querySelector(".chk-simbolos")
+const gerarSenha = document.querySelector(".gerar-senha")
+
+
+export default () => {
+gerarSenha.addEventListener("click", ()=>{
+senhaGerada.innerHTML = gera()
+})
+}
+
+function gera() {
+  const senha = geraSenha(
+    qtdCaracteres.value,
+    chkMaiusculas.checked,
+    chkMinusculas.checked,
+    chkNumeros.checked,
+    chkSimbolos.checked
+  )
+  return senha || 'nada selecionado'
+}
